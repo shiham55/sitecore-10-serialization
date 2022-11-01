e746c64bf12dc7cb7cfa5f6ebaed8216b07455d7
shiham <shiham.sham@wundermanthompson.com>
2022-11-01 16:21:04 +0800

    navigation project added

---
+++ src/Feature/Navigation/code/Controllers/MainNavigationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreDemo.Feature.Navigation.Controllers
+{
+    public class MainNavigationController : Controller
+    {
+        public ActionResult Header()
+        {
+            return View("~/Views/Feature/MainNavigation/Header.cshtml");
+        }
+
+        public ActionResult Footer()
+        {
+            return View("~/Views/Feature/MainNavigation/Footer.cshtml");
+        }
+    }
+}
